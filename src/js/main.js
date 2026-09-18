452e263ea20e3ef0ef1cd44a57e5be2bd342540a
giusmili <giusmili67@gmail.com>
2026-09-18 16:15:50 +0200

    first commit

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,16 @@
-const bouton = document.getElementById('btn-demo');
-const compteur = document.getElementById('compteur');
+document.addEventListener("DOMContentLoaded", (e)=> {
+  e.preventDefault();
+  const bouton = document.getElementById('btn-demo');
+  const compteur = document.getElementById('compteur');
+  const footer = document.getElementsByTagName('p')[3];
+  let clics = 0;
+  let dates = new Date().getFullYear();
+  footer.textContent = `© ${dates} - Tous droits réservés.`;
 
-let clics = 0;
+  bouton.addEventListener('click', () => {
+    clics += 1;
+    compteur.textContent = clics;
+  });
+  
 
-bouton.addEventListener('click', () => {
-  clics += 1;
-  compteur.textContent = clics;
-});
+})
