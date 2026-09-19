6ae2588517b9fe590d71bd6bd8742253986ea79b
giusmili <giusmili67@gmail.com>
2026-09-19 18:38:03 +0200

    com

--- src/js/main.js
+++ src/js/main.js
@@ -1,13 +1,18 @@
 document.addEventListener("DOMContentLoaded", (e)=> {
   /* fonction principale */
   e.preventDefault();
+
+  /* Eléments du DOM */
   const bouton = document.getElementById('btn-demo');
   const compteur = document.getElementById('compteur');
   const footer = document.getElementsByTagName('p')[3];
+
+  /* variables */
   let clics = 0;
   let dates = new Date().getFullYear();
   footer.textContent = `© ${dates} - Tous droits réservés.`;
-
+  
+  /* event */
   bouton.addEventListener('click', () => {
     clics += 1;
     compteur.textContent = clics;
