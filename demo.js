fb2da6b8dac04a87b7ddb71e4a8c745fc6006a73
giusmili <giusmili67@gmail.com>
2026-09-19 18:35:21 +0200

    js

--- demo.js
+++ demo.js
@@ -5,42 +5,38 @@ let nom = "John";
 console.log(nom);
 
 const person = {
-    name: "Alice",
-    age: 30,
-    ville: "Paris"
+  name: "Alice",
+  age: 30,
+  ville: "Paris",
 };
 
 console.info(typeof person);
 
-console.log(Object.keys(person)); 
+console.log(Object.keys(person));
 console.log(Object.values(person));
 let keys;
 
-for(keys in person) {
-    console.log(keys + " : " + person[keys]);
+for (keys in person) {
+  console.log(keys + " : " + person[keys]);
 }
 let date = new Date().getFullYear();
 console.log(date);
 
 /* ============================== */
 
-const teams =[
-
-    {
-        name: "Alice",
-    },
-    {
-        name: "Bob",
-    }  
-
-]
-console.log(teams
-        
-);
+const teams = [
+  {
+    name: "Alice",
+  },
+  {
+    name: "Bob",
+  },
+];
+console.log(teams);
 
 /* for (const team of teams) {
     console.log(team.name);
 } */
-teams.forEach((team)=> {
-    console.log("Nom : " + team.name);
+teams.forEach((team) => {
+  console.log("Nom : " + team.name);
 });
